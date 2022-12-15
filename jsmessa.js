1c31e4b0c9f591f8e7269d0d523f2669bb14460d
Dinara Zhaki <210103379@stu.sdu.edu.kz>
2022-12-15 18:03:19 +0600

    change

---
+++ jsmessa.js
@@ -0,0 +1,12 @@
+function headchange() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+function changeaw22(){
+  const element = document.getElementById("text1");
+  element.remove();
+}
