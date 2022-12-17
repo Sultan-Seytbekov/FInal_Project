9dcef93a82064084d5b2b43cfad6c8700bd363a4
Dinara Zhaki <210103379@stu.sdu.edu.kz>
2022-12-18 03:02:16 +0600

    not finished messa ;(

--- jsmessa.js
+++ jsmessa.js
@@ -10,5 +10,40 @@ function changeaw22(){
   document.getElementById("rs22").remove();
   document.getElementById("videocon").remove();
   document.getElementById("text1").innerHTML="Autumn/Winter22";
-  
+  document.getElementById("sl22").remove();
+  document.getElementById("aww22").remove();
+  document.getElementById("fs22").remove();
+  document.getElementById("ny21").remove();
+  document.getElementById("btshowaw").remove();
+  document.getElementById("btshowawrs").remove();
+  document.getElementById("btshowawsl").remove();
+  document.getElementById("btshowawfs").remove();
+  document.getElementById("btshowawny").remove();
+  document.getElementById("rsimg1").src="messaphotos/IMG_9049.JPG";
+  document.getElementById("rsimg2").src="messaphotos/IMG_9056.JPG";
+}
+let slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
